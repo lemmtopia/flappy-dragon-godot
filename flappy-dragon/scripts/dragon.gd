1c368d901b4d7dea6b6775a25c5b1205cb7b8d32
Gabriel G. Cunha <137636549+lemmtopia@users.noreply.github.com>
2026-01-13 10:45:14 -0300

    Start building.tscn

--- flappy-dragon/scripts/dragon.gd
+++ flappy-dragon/scripts/dragon.gd
@@ -1,19 +1,28 @@
 extends CharacterBody2D
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var sprite: Sprite2D = $Sprite
 
 @export var flap_strength : float = 150
 @export var gravity : float = 500
 
+var is_start_game : bool = true
+var can_control : bool = true
+
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	if Input.is_action_just_pressed("flap"):
-		velocity.y = -flap_strength
-	
-	if velocity.y < 0:
-		sprite_2d.frame = 1
-	else:
-		sprite_2d.frame = 0
-	
-	velocity.y = clamp(velocity.y, -flap_strength, flap_strength)
-	move_and_slide()
+	if is_start_game:
+		velocity.y += gravity * delta
+		if Input.is_action_just_pressed("flap") and can_control:
+			velocity.y = -flap_strength
+		
+		if velocity.y < 0:
+			sprite.frame = 1
+		else:
+			sprite.frame = 0
+		
+		velocity.y = clamp(velocity.y, -flap_strength, flap_strength)
+		move_and_slide()
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("obstacles"):
+		can_control = false
+		print("dead")
